b5620abb56ff7ae9f0bfdaf2bbcfb2a119cdf0f0
Seranqqq <144434050+Seranqqq@users.noreply.github.com>
2025-06-30 12:54:17 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var one=document.getElementById("menu")
+
+
+
+function suba(){
+   one.style.transform="translateX(0px)"
+   one.style.transition="all 1s ease-in-out"
+}  
+
+function seran(){
+   one.style.transform="translateX(-400px)"
+   one.style.transition="all 1s ease-in-out"
+}
+
